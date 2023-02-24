5e44d6f31cb5c25bcf49d71f4ea1aa8bf5e3aa67
strahinjaPC <strajo98@gmail.com>
2023-02-24 07:40:09 -0500

    adding old project to github

---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+    <footer>
+        <p>Copyright &copy;  2022</p>
+        <router-link to="/about">About</router-link>
+    </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    a{
+        color: #333;
+    }
+
+    footer{
+        margin-top: 30px;
+        text-align: center;
+    }
+</style>
